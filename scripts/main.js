7a6aeffbd48d2bbd060cc99041eac1c8c4fb17d1
Justin Scott <Jstman2000@gmail.com>
2021-05-16 21:33:37 -0400

    Animation and About Content
    Added nice scroll animations and content for about section

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,53 @@ $(document).ready(() => {
 
     $('.pagelink').click(function () {
         var anchor = $(this).attr('dest');
-        //$('.link-wrap').removeClass('visible');
-
-        //$('nav span').removeClass('active');
-        //$('nav')
-        //.find('[dest="' + anchor + '"]')
-        //.addClass('active');
         $('html, body').animate(
             {
                 scrollTop: $('#' + anchor).offset().top
             },
             400
         );
+    });
+
+    const scrollElements = document.querySelectorAll(".js-scroll");
+
+    const elementInView = (el, dividend = 1) => {
+      const elementTop = el.getBoundingClientRect().top;
+    
+      return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+      );
+    };
+    
+    const elementOutofView = (el) => {
+      const elementTop = el.getBoundingClientRect().top;
+    
+      return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+      );
+    };
+    
+    const displayScrollElement = (element) => {
+      element.classList.add("scrolled");
+    };
+    
+    const hideScrollElement = (element) => {
+      element.classList.remove("scrolled");
+    };
+    
+    const handleScrollAnimation = () => {
+      scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+          displayScrollElement(el);
+        } else if (elementOutofView(el)) {
+          hideScrollElement(el)
+        }
+      })
+    }
+    
+    window.addEventListener("scroll", () => { 
+      handleScrollAnimation();
     });
 
 
